b77892f03236abb0fe76f05979e752bdecb51bd1
Ted Irland <irlandth@gmail.com>
2020-04-21 18:14:08 -0500

    Attempt to troubleshoot HTML routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +17,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
+
 app.use(require("./routes/view.js"));
-app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
