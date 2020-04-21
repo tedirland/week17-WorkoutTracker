5bc99802d45891c369d371ccf6b9ca6e59be9068
Ted Irland <irlandth@gmail.com>
2020-04-21 05:51:45 -0500

    Pushing with Model inserted - pushing to MongoDB

---
+++ Develop/models/workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: Date,
+     
+    exercises: [
+        {
+          type: String,
+          name: String,
+          duration: Number,
+          weight: Number,
+          reps: Number,
+          sets: Number,
+          distance: Number
+        }
+    ]
+ 
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
