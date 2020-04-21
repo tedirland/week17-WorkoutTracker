2166919014ba086269769d90186143283038bd81
Ted Irland <irlandth@gmail.com>
2020-04-21 14:29:56 -0500

    Updated API Routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const db = require("../models")
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,26 @@
-const db = require("../models")
+const router = require("express").Router()
+const Workout = require("../models/workout")
 
 //Serving up the get route for workouts
 
-module.exports = function (app) {
-    app.get("api/workouts", function (req, res) {
-        db.Workout.find({}).then(function(dbExercises) {
-            res.json(dbExercises)
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction)
+        })
+        .catch(err => {
+            res.status(400).json(err)
         });
-    });
+});
 
-    // Serving up the workouts range call
-    app.get("api/workouts/range", function (req, res) {
-       // Mongoose query that selects during a certian range based on user input
-    });
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbTransaction => {
+            res.json(dbTransaction)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
+});
 
-    
-};
+module.exports = router;
