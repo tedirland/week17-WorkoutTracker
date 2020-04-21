2fa5ebf6a8b6da3d77092157f10b8e1c19a9102b
Ted Irland <irlandth@gmail.com>
2020-04-21 18:25:07 -0500

    Fixed dirname Issue

--- Develop/routes/view.js
+++ Develop/routes/view.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname), "../public/exercise.html")
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname), "../public/stats.html")
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 module.exports = router;
